3987f630bb1ae62892b90bcbaa2f500ea89f05a4
renanprado <renanprado96@gmail.com>
2019-09-28 12:41:32 -0300

    create login ui

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,8 +1,51 @@
+<script>
+/* eslint-disable */
+
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an Login page ui</h1>
+  <div class="main">
+    <div class="container">
+      
+      <div class="login">
+
+        <div class="login__container">
+          
+          <!-- Header -->
+          <div class="login__header">
+            <h1> Entrar </h1>
+          </div>
+
+          <!-- Content -->
+          <div class="login__content">
+
+            <div class="login__content-section">
+              <input type="email" placeholder="Digite seu email" />
+            </div>
+
+            <div class="login__content-section">
+              <input type="password" placeholder="Digite sua senha" />
+            </div>
+
+            <div class="login__content-section">
+              <button> Entrar </button>
+            </div>
+
+          </div>
+
+          <!-- Footer -->
+          <div class="login__footer">
+            <router-link to="/signup">Criar uma conta</router-link>
+          </div>
+
+        </div> <!-- ./ login__container -->
+
+      </div> <!-- ./ login -->
+
+    </div> <!-- ./ container -->
   </div>
 </template>
+
 
 <style lang="scss">
   @import "../styles/pages/login";
