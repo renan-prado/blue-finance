e7d4335060dcd208a02c17c114644a58d8910ee0
renanprado <renanprado96@gmail.com>
2019-09-30 04:26:13 -0300

    create financeitem and finance view

---
+++ src/components/FinanceItem.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="finance-view__item">
+      <div :class="{'finance-view__item-variation': true, '--red': variation < 0 }"> <label>{{ variation_set }} </label> </div>
+      <div class="finance-view__item-value"> <label> {{ value_set }} </label> </div>
+      <div class="finance-view__item-text"> <h2> {{ title_set }} </h2> </div>
+    </div> <!-- ./ finance-view__item -->
+</template>
+
+<script>
+
+export default {
+  name: 'Financeitem',
+
+  data(){
+    return {
+    }
+  },
+
+  computed: {
+
+    variation_set: function(){
+      return this.variation ? this.variation + '%' : '';
+    },
+
+    value_set: function(){
+      return this.value ? 'R$ ' + this.value : '';
+    },
+
+    title_set: function(){
+      return this.title ? this.title : '';
+    },
+
+  },
+
+  props: ['variation', 'value', 'title']
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  @import "../styles/components/financeView.scss";
+</style>
--- src/components/FinanceView.vue
+++ src/components/FinanceView.vue
@@ -7,37 +7,15 @@
             <!-- <h1> Cotações </h1> -->
 
             <div class="finance-view__list">
+
+              <Financeitem variation="-0.344" value="2.90" title="Dólar" />
+              <Financeitem variation="0.273" value="2.94" title="Euro" />
+              <Financeitem variation="-0.876" value="1.28" title="Peso Argentino" />
+              <Financeitem variation="0.099" value="2.57" title="Libra Estelina" />
+              <Financeitem variation="0.983" value="3.77" title="Bitcoin" />
+              <Financeitem variation="0.204" value="1.93" title="Bovespa" />
+              <Financeitem variation="-0.099" value="3.24" title="Não sei" />
               
-              <div class="finance-view__item">
-                <div class="finance-view__item-variation"> <label>-0.245%</label> </div>
-                <div class="finance-view__item-value"> <label> R$ 4,16 </label> </div>
-                <div class="finance-view__item-text"> <h2> Peso Argentino </h2> </div>
-              </div> <!-- ./ finance-view__item -->
-
-              <div class="finance-view__item">
-                <div class="finance-view__item-variation"> <label>-0.245%</label> </div>
-                <div class="finance-view__item-value"> <label> R$ 4,16 </label> </div>
-                <div class="finance-view__item-text"> <h2> Peso Argentino </h2> </div>
-              </div> <!-- ./ finance-view__item -->
-
-              <div class="finance-view__item">
-                <div class="finance-view__item-variation"> <label>-0.245%</label> </div>
-                <div class="finance-view__item-value"> <label> R$ 4,16 </label> </div>
-                <div class="finance-view__item-text"> <h2> Peso Argentino </h2> </div>
-              </div> <!-- ./ finance-view__item -->
-
-              <div class="finance-view__item">
-                <div class="finance-view__item-variation"> <label>-0.245%</label> </div>
-                <div class="finance-view__item-value"> <label> R$ 4,16 </label> </div>
-                <div class="finance-view__item-text"> <h2> Peso Argentino </h2> </div>
-              </div> <!-- ./ finance-view__item -->
-
-              <div class="finance-view__item">
-                <div class="finance-view__item-variation"> <label>-0.245%</label> </div>
-                <div class="finance-view__item-value"> <label> R$ 4,16 </label> </div>
-                <div class="finance-view__item-text"> <h2> Peso Argentino </h2> </div>
-              </div> <!-- ./ finance-view__item -->
-
             </div> <!-- ./ finance-view__list -->
 
           </div> <!-- ./ finance-view__group -->
@@ -50,12 +28,18 @@
 
 <script>
 
+import Financeitem from "./FinanceItem";
+
 export default {
   name: 'FinanceView',
 
   data(){
     return {
     }
+  },
+
+  components: {
+    Financeitem,
   }
 };
 </script>
